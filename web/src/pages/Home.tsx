58cf101fec6934c10f89ad49e230ae48b26315d4
William Milanez <william.milanez@outlook.com>
2026-05-01 13:41:54 -0300

    test: validate flashcard creation via api service

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,9 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { flashcardService } from "../services/flashcard.service";
 
 export function Home() {
+  const hasExecuted = useRef(false);
+
   useEffect(() => {
-    flashcardService.getAll().then(console.log).catch(console.error);
+    if (hasExecuted.current) return;
+    hasExecuted.current = true;
+
+    flashcardService
+      .create({
+        question: "Teste",
+        answer: "Resposta",
+        category: "JavaScript",
+      })
+      .then(console.log)
+      .catch(console.error);
   }, []);
 
   return <div>Home</div>;
