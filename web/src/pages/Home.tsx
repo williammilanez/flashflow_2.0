7e09f2700bc1c7f0a8f1aa4706c471f18887cb72
William Milanez <william.milanez@outlook.com>
2026-05-01 13:43:09 -0300

    refactor(frontend): remove temporary api test logic from home page

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,22 +1,3 @@
-import { useEffect, useRef } from "react";
-import { flashcardService } from "../services/flashcard.service";
-
 export function Home() {
-  const hasExecuted = useRef(false);
-
-  useEffect(() => {
-    if (hasExecuted.current) return;
-    hasExecuted.current = true;
-
-    flashcardService
-      .create({
-        question: "Teste",
-        answer: "Resposta",
-        category: "JavaScript",
-      })
-      .then(console.log)
-      .catch(console.error);
-  }, []);
-
   return <div>Home</div>;
 }
